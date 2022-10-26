c531b83552e7f12b76d390d346e7d52fbdb53667
Shivam Jha <86483059+ShivamJhaa@users.noreply.github.com>
2022-10-26 19:22:32 +0530

    test

--- core/tests/webdriverio_desktop/publicationAndLibrary.js
+++ core/tests/webdriverio_desktop/publicationAndLibrary.js
@@ -190,10 +190,12 @@ describe('Library index page', function() {
     // Private explorations are not shown in the library.
     await libraryPage.expectExplorationToBeHidden('Vilya');
 
+    await libraryPage.selectLanguages([LANGUAGE_FRANCAI]);
     await libraryPage.findExploration(EXPLORATION_VINGILOT);
     // The first letter of the objective is automatically capitalized.
     expect(await libraryPage.getExplorationObjective(EXPLORATION_VINGILOT))
       .toBe('Seek the aid of the Valar');
+    await libraryPage.selectLanguages([LANGUAGE_DEUTSC]);
     await libraryPage.findExploration(EXPLORATION_SILMARILS);
     await libraryPage.playExploration(EXPLORATION_SILMARILS);
     await explorationPlayerPage.expectExplorationNameToBe('silmarils');
