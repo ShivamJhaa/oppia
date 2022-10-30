6b7f63994074e5c2bf78f7471082380b80e09e03
Shivam Jha <86483059+ShivamJhaa@users.noreply.github.com>
2022-10-31 00:01:18 +0530

    test

--- core/tests/webdriverio_desktop/explorationHistoryTab.js
+++ core/tests/webdriverio_desktop/explorationHistoryTab.js
@@ -725,7 +725,6 @@ describe('Exploration history', function() {
     // Revert to version 2.
     await explorationEditorPage.navigateToHistoryTab();
     await explorationEditorHistoryTab.revertToVersion(2);
-    await browser.debug();
     await explorationEditorHistoryTab.expectRevertToVersion(2);
 
     // Verify exploration is version 2.
